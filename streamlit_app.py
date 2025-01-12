3ebb24cc868ede72a5446874609e6d719c07ec7a
Andrew Crisp <acrisp013@gmail.com>
2025-01-12 00:48:55 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if ingredients_list:
 
 
 import requests
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

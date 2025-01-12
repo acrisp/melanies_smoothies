891df28ec7544a1da4b0bbd729bf37908fd5572b
Andrew Crisp <acrisp013@gmail.com>
2025-01-12 00:42:40 -0500

    next step

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,12 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -47,6 +47,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
